202a6619e78c1ba7f89fe0842127cd81a31caa9a
Ios12316 <idowuolakunlesamuel@gmail.com>
2026-04-29 21:05:21 +0100

    Updated project files

--- src/controllers/courseController.js
+++ src/controllers/courseController.js
@@ -1,5 +1,6 @@
 const Course = require('../models/Course');
-exports.getAllCourses = async (req,res) => {
+
+exports.getAllCourses = async (req, res) => {
     try {
         const courses = await Course.find().populate('student');
         res.status(200).json(courses);
@@ -16,7 +17,7 @@ exports.getCourseById = async (req, res) => {
         res.status(500).json({message: error.message});
     }
 }
-exports.createCourse = async (req,res)=>{
+exports.createCourse = async (req, res)=>{
     try{
         const newCourse = await Course.create(req.body);
         res.status(201).json(newCourse);
@@ -24,7 +25,7 @@ exports.createCourse = async (req,res)=>{
         res.status(400).json({message: error.message});
     }
 }
-exports.deleteCourse = async (req,res)=>{
+exports.deleteCourse = async (req, res)=>{
     try {
         const deletedCourse = await Course.findByIdAndDelete(req.params.id);
         if(!deletedCourse) return res.status(404).json({message: 'Course not found'});
--- src/routes/studentRoutes.js
+++ src/routes/studentRoutes.js
@@ -1,5 +1,6 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const {getAllStudents, getStudentById, createStudent, updateStudent, deleteStudent} = require('../controllers/studentController');
 router.get('/', getAllStudents).get('/:id', getStudentById).post('/', createStudent).put('/:id', updateStudent).delete('/:id', deleteStudent);
 module.exports = router;
+
