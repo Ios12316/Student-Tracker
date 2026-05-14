849cee6abe5a30c302d23d12772dd11a079bf1dd
Ios12316 <idowuolakunlesamuel@gmail.com>
2026-05-14 13:13:08 +0100

    add new codes

--- src/index.js
+++ src/index.js
@@ -20,6 +20,10 @@ app.use(cors({
 app.use("/students", studentRoutes);
 app.use("/courses", courseRoutes);
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`✅ Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== "production") {
+    app.listen(PORT, () => {
+        console.log(`✅ Server running on port ${PORT}`);
+    });
+
+}
+module.exports = app;
